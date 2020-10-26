27b7403bdc1f4e261e90239dba1a807c72010e4e
seblkma <sebmalikkeung@gmail.com>
2020-10-25 23:12:56 -0700

    added AWS ubuntu deployment

--- sqliteserver/deployaws.sh
+++ sqliteserver/deployaws.sh
@@ -1,2 +1,3 @@
+# for security reason, pem file is not checked in
 scp -i ~/dev/aws/martin/free-instance-seb.pem survey ubuntu@ec2-46-137-199-71.ap-southeast-1.compute.amazonaws.com:~/staging/sqliteserver/survey
 scp -i ~/dev/aws/martin/free-instance-seb.pem -r templates ubuntu@ec2-46-137-199-71.ap-southeast-1.compute.amazonaws.com:~/staging/sqliteserver
